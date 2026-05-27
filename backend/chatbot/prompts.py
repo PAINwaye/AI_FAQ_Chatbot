1e59c679af2559fd1cecede2e2c7ffcd8eb9580f
PAINwaye <adhithya30082004@gmail.com>
2026-05-27 12:02:47 +0530

    no of faq increased

--- backend/chatbot/prompts.py
+++ backend/chatbot/prompts.py
@@ -13,13 +13,7 @@ RULES:
 - Keep formatting neat
 - Do not overuse emojis
 - Make answers visually clean
-- Provide minimum 15 FAQ for each topic
-- Always ask user if they want answers elaborately or midium length or short answers and format the answers accordingly
-- If user wants elaborately answers, provide detailed explanations and examples for each FAQ
-- If user wants medium length answers, provide concise explanations and examples for each FAQ
-- If the answer requires code snippets, format them properly and do not show raw markdown symbols
-- If the answer needs to be presented in a tabular format, format it properly and do not show raw markdown symbols
-
+- Provide minimum 20 FAQ for each topic
 
 FORMAT:
 
